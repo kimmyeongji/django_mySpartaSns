42ead4eb951da5bc8d21dd454b4a487e9c1c1a19
myeongji <myeongji0888@gmail.com>
2022-09-27 17:52:21 +0900

    Aplied django default authenticate function (sparta django 3-6)

--- user/views.py
+++ user/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import UserModel
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model #사용자가 데이터베이스 안에 있는지 검사하는 함수
+from django.contrib.auth import auth
 
 
 # Create your views here.
@@ -13,19 +15,19 @@ def sign_up_view(request):
         password2 = request.POST.get('password2', None)
         bio = request.POST.get('bio', None)
 
-        if username == UserModel.objects.get(username=username):
+        user_count = UserModel.objects.filter(username=username).count()
+        if user_count > 0:
             return HttpResponse("기존에 가입한 회원과 이름이 동일합니다!")
 
         if password != password2:
             return render(request, 'user/signup.html')
         else:
-            new_user = UserModel()
-            new_user.username = username
-            new_user.password = password
-            new_user.bio = bio
-            new_user.save()
-
-        return redirect('/sign-in/')
+            exist_user = get_user_model().objects.filter(username=username)
+            if exist_user:
+                return render(request, 'user/signup.html')
+            else:
+                UserModel.objects.create_user(username=username, password=password, bio=bio)
+                return redirect('/sign-in/')
 
 
 def sign_in_view(request):
@@ -33,9 +35,10 @@ def sign_in_view(request):
         username = request.POST.get('username',None)
         password = request.POST.get('password',None)
 
-        me = UserModel.objects.get(username=username)
-        if me.password == password:
-            request.session['user'] = me.username
+        me = auth.authenticate(request, username=username, password=password)
+
+        if me is not None:
+            auth.login(request, me)
             return HttpResponse(f"{username} 로그인 성공!")
         else:
             return redirect('/sign-in/')
